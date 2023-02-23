d779bb4a612c2a77d7a16ee6401e45033b261026
Crowdbotics <team@crowdbotics.com>
2023-02-23 17:35:08 +0000

    Update admin title

--- backend/feature_logic_enric_39040/urls.py
+++ backend/feature_logic_enric_39040/urls.py
@@ -34,9 +34,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Feature logic enrich"
-admin.site.site_title = "Feature logic enrich Admin Portal"
-admin.site.index_title = "Feature logic enrich Admin"
+admin.site.site_header = "Enrichment Priority 1"
+admin.site.site_title = "Enrichment Priority 1 Admin Portal"
+admin.site.index_title = "Enrichment Priority 1 Admin"
 
 # swagger
 urlpatterns += [
